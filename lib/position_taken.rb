09772f3ccb964947dd943b86e549fc32cb67cb8f
Ryan McCahon <mccahonr@yahoo.com>
2020-08-17 19:47:28 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,6 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  if index = " "
-    false
-  elsif index = ""
+  if index = " " || index = ""
     false
   else
     true
