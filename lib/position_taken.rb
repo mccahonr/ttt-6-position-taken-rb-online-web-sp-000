a1000610236fa7c192d02c35ee469373836a4a14
Ryan McCahon <mccahonr@yahoo.com>
2020-08-17 19:45:07 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,4 +1,9 @@
 # code your #position_taken? method here!
-def position_taken?
+def position_taken?(board, index)
+  if index == " " || index == "" || index == nil
+    false
+  else
+    true
+  end   
 
 end
